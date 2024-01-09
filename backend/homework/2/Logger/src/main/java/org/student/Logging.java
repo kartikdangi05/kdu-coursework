d3de3d4d61a200c275f11620e049110e7a756ff5
kartikdangi05 <kartik.dangi@kickdrumtech.com>
2024-01-09 22:12:18 +0530

    removed code smells -2

--- backend/homework/2/Logger/src/main/java/org/student/Logging.java
+++ backend/homework/2/Logger/src/main/java/org/student/Logging.java
@@ -4,5 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Logging {
-    public static final Logger logger = LoggerFactory.getLogger(Logging.class);
+
+    private Logging(){
+
+    }
+    public static final Logger logger = LoggerFactory.getLogger(org.student.Logging.class);
 }
